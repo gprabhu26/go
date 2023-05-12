18e5c86f1c1071776b0d1fde9820367c2229ce3b
gprabhu26 <gprabhu14@gmail.com>
2023-05-12 22:47:03 +0530

    inputfromuser
    input from user

--- firstprogram.go
+++ firstprogram.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func mainx() {
 
 	fmt.Println("First Go Program")
 
---
+++ inpufromuser.go
@@ -0,0 +1,20 @@
+package main
+
+// fmt package provides the function to print anything
+import "fmt"
+
+func main() {
+
+	// declaring the variable using the var keyword
+	var numberFromUser int
+	fmt.Println("Please enter the number you want to check that it is divisible by 2 or not:")
+
+	// scanning the input by the user
+	fmt.Scanln(&numberFromUser)
+	// logic to check that number is divisible by 2 or not
+	if numberFromUser%2 == 0 {
+		fmt.Println(numberFromUser, "is divisible by 2")
+	} else {
+		fmt.Println(numberFromUser, "is not divisible by 2")
+	}
+}
