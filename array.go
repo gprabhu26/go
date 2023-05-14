f1ef3769bbb17fd417420b494c0c2dc9d9eb0bf9
gprabhu26 <gprabhu14@gmail.com>
2023-05-14 12:18:29 +0530

    array
    array

---
+++ array.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func arraymain() {
+	// Declare an array of integers with a fixed length of 3
+	var myArray [3]int
+
+	// Assign values to the elements of the array
+	myArray[0] = 1
+	myArray[1] = 2
+	myArray[2] = 3
+
+	// Print the array
+	fmt.Println(myArray) // [1 2 3]
+}
